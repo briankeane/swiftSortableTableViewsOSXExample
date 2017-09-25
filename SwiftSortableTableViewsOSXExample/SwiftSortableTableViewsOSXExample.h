895498c81d9c988e997e282407ef60bc35163c67
briankeane <lonesomewhistle@gmail.com>
2017-09-25 15:31:05 -0500

    initial commit

---
+++ SwiftSortableTableViewsOSXExample/SwiftSortableTableViewsOSXExample.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSortableTableViewsOSXExample.h
+//  SwiftSortableTableViewsOSXExample
+//
+//  Created by Brian D Keane on 9/25/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftSortableTableViewsOSXExample.
+FOUNDATION_EXPORT double SwiftSortableTableViewsOSXExampleVersionNumber;
+
+//! Project version string for SwiftSortableTableViewsOSXExample.
+FOUNDATION_EXPORT const unsigned char SwiftSortableTableViewsOSXExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSortableTableViewsOSXExample/PublicHeader.h>
+
+
